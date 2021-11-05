7ff3031696509450287b931387aae3b4a180fdf8
edgarperez97 <edgarperez_97@hotmail.com>
2021-11-04 21:52:21 -0600

    se modifico el script de word

--- Word/archivos_no_sanos/word_creator_x.py
+++ Word/archivos_no_sanos/word_creator_x.py
@@ -1,15 +1,42 @@
-#CREAR 100 ARCHIVOS WORD#
+#CREAR 25 ARCHIVOS WORD#
+import random
 from docx import Document
 from docx.shared import Cm
 
+nombres = (
+    'Final_Poo_tarea_',
+    'Labs_POO_tarea_',
+    'Telecomunicaciones1_tarea_',
+    'Telecomunicaiones2_tarea_',
+    'lab_Tele3_tarea_',
+    'lab_tele4_tarea_',
+    'Programacion1_tarea_',
+    'IngenierriaaSocial_tarea_',
+    'SistemasOperativaos_tarea_',
+    'SDS_tarea_',
+    'segBasesDatos_tarea_',
+    'BasesDatos_tarea_',
+    'SEGA_tarea_',
+    'LabSEGA_tarea_',
+    'Coco_tarea_',
+    'Criptografia_tarea_',
+    'DiseñoArquitecturas_tarea_',
+    'NTSI_tarea_',
+    'SegCompNube_tarea_'
+)
+
+def elegir_materia():
+    materia = random.choice(nombres)
+    materia.encode()
+    return materia
 
 def main():
-    for iteracion in range(1, 101):
+    for iteracion in range(1, 26):
         # Creación del documento
         document = Document()
 
         # Añadimos un titulo al documento, a nivel 0
-        titulo_documento = "Documento"+str(iteracion)+"creado con Python"
+        titulo_documento = "Documento"+str(iteracion)+" creado con Python"
         document.add_heading(titulo_documento, 0)
 
         # Añadimos un párrafo
@@ -50,7 +77,8 @@ def main():
             row_cells[0].text = prod
             row_cells[1].text = str(numbr)
 
-        nombre_documento = "ejemplo_"+str(iteracion)+".docx"
+        nombre_documento = elegir_materia()
+        nombre_documento = nombre_documento + str(iteracion)+".docx"
 
         document.save(nombre_documento)
 
--- Word/archivos_sanos/word_creator.py
+++ Word/archivos_sanos/word_creator.py
@@ -1,7 +1,34 @@
 #CREAR 100 ARCHIVOS WORD#
+import random
 from docx import Document
 from docx.shared import Cm
 
+nombres = (
+    'Final_Poo_tarea_',
+    'Labs_POO_tarea_',
+    'Telecomunicaciones1_tarea_',
+    'Telecomunicaiones2_tarea_',
+    'lab_Tele3_tarea_',
+    'lab_tele4_tarea_',
+    'Programacion1_tarea_',
+    'IngenierriaaSocial_tarea_',
+    'SistemasOperativaos_tarea_',
+    'SDS_tarea_',
+    'segBasesDatos_tarea_',
+    'BasesDatos_tarea_',
+    'SEGA_tarea_',
+    'LabSEGA_tarea_',
+    'Coco_tarea_',
+    'Criptografia_tarea_',
+    'DiseñoArquitecturas_tarea_',
+    'NTSI_tarea_',
+    'SegCompNube_tarea_'
+)
+
+def elegir_materia():
+    materia = random.choice(nombres)
+    materia.encode()
+    return materia
 
 def main():
     for iteracion in range(1, 101):
@@ -9,7 +36,7 @@ def main():
         document = Document()
 
         # Añadimos un titulo al documento, a nivel 0
-        titulo_documento = "Documento"+str(iteracion)+"creado con Python"
+        titulo_documento = "Documento"+str(iteracion)+" creado con Python"
         document.add_heading(titulo_documento, 0)
 
         # Añadimos un párrafo
@@ -50,7 +77,8 @@ def main():
             row_cells[0].text = prod
             row_cells[1].text = str(numbr)
 
-        nombre_documento = "ejemplo_"+str(iteracion)+".docx"
+        nombre_documento = elegir_materia()
+        nombre_documento = nombre_documento + str(iteracion)+".docx"
 
         document.save(nombre_documento)
 
