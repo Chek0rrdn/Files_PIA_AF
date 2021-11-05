e8c854c68d9c3cdb960725959d0f61d537970cd1
edgarperez97 <edgarperez_97@hotmail.com>
2021-11-05 01:40:59 -0600

    se modifico png

--- Png/archivos_no_sanos/png_creator_x.py
+++ Png/archivos_no_sanos/png_creator_x.py
@@ -1,10 +1,14 @@
 #CREAR 10 IMAGENES#
+import os, random
 import shutil
 
 def main():
 
-    for iterador in range(1, 11):
-        shutil.copy2('imagenpenege.PNG', f'imagenpenege_{iterador}.PNG')
+    archivos = os.listdir('./imagenes')
+    
+    for i in range(1, 11):    
+        imagen_seleccionado = random.choice(archivos)
+        shutil.copy2(f'./imagenes/{imagen_seleccionado}', f'imagen_seleccionado_{i}.png')
 
 if __name__ == '__main__':
 	main()
