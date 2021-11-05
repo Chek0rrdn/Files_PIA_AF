4a6b9077cad1c33e2227a02cfe3a05cdf4accfa2
edgarperez97 <edgarperez_97@hotmail.com>
2021-11-05 02:42:11 -0600

    se agrego el archivo para mp3

---
+++ Mp3/mp3creator.py
@@ -0,0 +1,33 @@
+#CREAR 50 CANCIONES MP3#
+import random
+import shutil
+
+nombres_Canciones = (
+    'Shepherd_of_Fire',
+    'Hail_to_the_King',
+    'Doing_Time',
+    'This_Means_War',
+    'Requiem',
+    'Crimson_Day',
+    'Heretic',
+    'Coming_Home',
+    'Planets',
+    'Acid_Rain',
+    'This_Means_War'
+)
+
+def elegir_cancion():
+    cancion = random.choice(nombres_Canciones)
+    cancion.encode()
+    return cancion
+
+
+def main():
+    ruta = "./cancion/Carry_On.mp3"
+    
+    for i in range(1, 51):    
+        cancion_seleccionada = elegir_cancion()
+        shutil.copy2(f'./cancion/Carry_On.mp3', f'{cancion_seleccionada}_{i}.mp3')
+
+if __name__ == '__main__':
+	main()
