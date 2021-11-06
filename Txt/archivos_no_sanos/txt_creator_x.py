350e3fc381e0b4cd5630248674ae2247be73da0a
edgarperez97 <edgarperez_97@hotmail.com>
2021-11-06 01:34:53 -0600

    se modifico el txt_creator_x

--- Txt/archivos_no_sanos/txt_creator_x.py
+++ Txt/archivos_no_sanos/txt_creator_x.py
@@ -44,33 +44,86 @@ nombres = (
     'SegCompNube_nota_'
 )
 
-rsapr = (
-	'-----BEGIN RSA PRIVATE KEY-----MIIBPAIBAAJBAJQ8RHiLUk1SNSgtMRdEsvRCM03v6HHIRKGKPEiPR+vtpEaEpsnmjH2aFJXBcWsbbIbouMbBRVb/5DbKFRvcgWMCAwEAAQJAZuBIuMwgK09RWve6KSbhd4l2J7HHRSgzEVVprwXZaU9pwzumPYXD9uo8N7D3YP+gyd42cnJA3cH634A8P04CgQIhAOte+Sr/HO+QGg+hdRiPxfniNOn2TfsmyRJXlpvsdYfBAiEAoTo7AUPOLjqJMWn+iHOM3fP/EIAgx7s/6AAWb/8XciMCIQDIgHsZmYD4wnSw5AVwCunHWF9i/qKnU/7tVRfDc8cbAQIhAIltDxp252n0LzjVdkZs5+fQsNspu8dDzqc8W+O+9gctAiEA2svIEsBPFcJ47OTkHjC2w56ADACn/HOkQDkqqVcg+pY=-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOwIBAAJBANIrqttdUgmdutgNfZOSnrYKQ0qZLSneYXbXDLkKOIZotpGVfrq9SJ0gVbOMorgotd5bovnAtHhSSuGzn5tcst0CAwEAAQJBAMIrC6+cmMU/jrY6SHEFPVHLS33o5BvRUtQbO6rccXpiKDETj/wlUd8QgBlr5ewDr043f3xclhtCGbGkYprQ+yECIQD1xbYYgxu017slBNLcj69dbtAYOX7+9FfsxDiX62H2pQIhANrqrgXl0em8T8uRPjXDkT3OtDjU7o+i73e+LazvJ03ZAiBRfJEIhYfNKO3as0O6b0VkhBYowf9yRs9UKCbfb+KWJQIgY+7vg/e2jGmPT5E9AdQF0F40vSDOyjkJ8fKZ7TC7E3kCIQCR1FtaO7Gg1mhc9WC4ImG4KZ84w8A4gIlLKK22qUHFcA==-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOAIBAAJATsxJwkTb7MRH4ajZaGfuSLqMAKLs9XeWJN9K3V2APWweRyCHGEDt0E4FpmVTKYqkXO/lJGqY7tzl3RteCh11WQIDAQABAkArSMy7ilfEl41kx/uk/nJGQTLmppjDvg0b9AyLzPDEzCMmLSGAW8XErQlGlP9pK0TkJmn1QkjwBi35w04C4qABAiEAlFgI+jYGJ58LucQjki1cKjwizCB/yvzK3s6bNXJGDXkCIQCH+7gmY0XhfvMCBCzGr9/e01oU8zf1aPl3Pe/VQ4cO4QIgePwbxkWNW38hhw5qnGuWQXPuRWE0GI93vaPvffWHILECICTC2vydt8WHIUC5SjWIVDgT4PVq7wTMFSy0NMLtl3RhAiAmNiLxbwKOskA128zkPbMq1pjPMqshL1ohQuQJ1M6t4A==-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOQIBAAJBAKhzZo7e4K0ktWjdAMjDubUKDijv0aVwIOkWagQYIZkjcRKLFzMd/AbH0WnmI0DdsnwT6JntcfN5qqRmb1RAbIkCAwEAAQJAKcsX8vKRQ+QLrkzG2QjWlng8MyPtG+LKB4qsGv/xthCRcgwrTJg6nFSeXcDczoT5aUjIE8xbZ3NpYj0y7j8AQQIhAOYpuAIpq5eW6yrDxWIZslFn/3YZMX9HMjMIyA8fCq+dAiEAu1w8O2CtSbnfIKkJec9qOyGFuIYUkZDnKa4Ht6DOet0CIGDkLR9T9XVyRfuRwJUkpimY8zT/NBQVn69WQgoDklcNAiBzeX2F+yfScMSclJJKiROoLhkEiV7T+MMAZUpMWsfn4QIgQsaO4JUcFZs9M6pXVsvAQgTykkN7D4pqFTXPjyNV95k=-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOgIBAAJBAIJbjCYE1fsQ0WV2u8YXkqnELULCg7FekgbfTxKRzWmpu+HoU8lLWOXfoYZIDuZ4eSMEqcSz1MeB5K+nqebvwzUCAwEAAQJAeCC5NEf8kRNchC44uEH86+lW9FCgX5GQ7DhZe/lU3GsrAFT3Rnm0jd2ICpztdHdkybbql/Ze5dFXL0PmONgP+QIhAO+3NrBdd/trDwkR3+Fqu1CmGlA+zK5qT71H64DY+88TAiEAizaJjsFI6X18JDVYkeu5qPiBCfKvKOO8RIyBqqqVxZcCIGhFPpGqppUDNZARaVvpDD+BcPK0zl2Tusmvrf3m5GlnAiEAiLpMyk+d++1KMwnN7qt1krZXjlyzmlZp/218oRGfvgsCIAXhdnpRLtY1iLipjM2vpLjg35z0dsILkSNaKxKAnZUH-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBPAIBAAJBANx7pqnpDKcq1gW2IsNfu2LszZC7jBiMgMRHdNG3ZB46LZE44MTOKPQsG8Px2m01AL/1cB+W7x5dG8DnlyAW3y0CAwEAAQJBAKlZiIAFcySLcGxeo4DxbRrkzFgY1didQuKACCh09j+H/t9PG+j2gKBS/q8DAWCQ9jhfvL5L0f7ctzD1KABDqkECIQD38R5rfdgJmgjYREEh3/t+qqqT3bOS7nKrXEQ0zm39SQIhAOOmEu90K6VIZqnSSuUFwybMte1n6mB0o6WcWCcVqUbFAiEAyd6gZOZ2MgMe9m5EFX+W9V+hOHkGjzfjVw/tXI1CUjkCICovz5iCbcgud6AG8YwpmjWpgBljxYJkeyMoqwlrMTiFAiEA9Rf8zLtTdBprOSp/dYPt7CjCrffCyMgPHCT7/c1cvfg=-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOgIBAAJBAIugtJEHPYnjR4WK2tb/D9EQQ90fRGssSaMv28BeELMOo+jZi/MbpwWSCgMoTA4bgKCK2ugyqSXr9ySMLPfW1JkCAwEAAQJAZs5W5QnNt78OpDcO1opKnQY60Y76E2cyeOL+DG+2H/+nrQLdhmJQohIE/6Sk6T3EQEmZKWXr5q0CDIBezSLiAQIhAN11ry2sdLo/NDGr/Mu4jMpTU1azXvlbUVRBhlCdE9l5AiEAoWeumrT1uU0bKbQpk+60w54XTkn9OFBVjtXlPLNfLCECIQC+ZgSjVwbQbbCZGA3ytItPFA6tlu+LoUguEVvCCRpcuQIgaI+adW61oQ1qg2HLbaI+SFuqDXWxLUIdqyK2FtS82wECIBmTXHuDBwn5MJmsNkPJy1EN1zpUbGTaYhizz0DtyRKC-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOQIBAAJAcqC3EkYw+k5Wy1MLZW8L4Tr6Wf7UaBdbf9NiX5bPFtvrAAYasxkfMx97Ob8GM6uzjyshgSaMBoBQhh9gQWiUEwIDAQABAkAftWePDY2k4CDQU+OWJpbBsj/ovz5goP11qbLPwHb+2mtfcP19ynbtlp6nvXDW7tnoRQfKyk5u8ZHSbYeovZAhAiEA2lBGqci0GbuSuT1PjhYCHXXA3Rp4c7FHULOwlZb+IeMCIQCGalocDSWmciXavDKSOGl6qRGzA3p+YcpScbLlaDacEQIgMiP11tDt7GsUS881EmEjoziET0PhHlE9G9/pUrIXNBkCIDNob4g0+wZat+GdgtXrn2PbT7qghPPbIM9BTwNwRRqRAiEAv8INUQhUkL9XEj5etKHdbGqhnuUQo004qRXG4jDYHR4=-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOQIBAAJAVJ9w7ATBXuZ6gkmICff/8UwqzvJFqVV94w3Up9aXhXrsgIFZle+UQ9yYKykfoCEs27yvjfq3nBn5nxqnL1TqswIDAQABAkA0ttHIJLo0nMxqBtY4p4WHUdRE8ATXFEuEOmRjXTksS4s6kZ/3S0t9wg5D1YyKIGBPnlFAH+IHB+z27dIx+ytZAiEApCWFn/wiaj2pP+1IeYHmkSUb1OxKnUoCg0kbAul1ceUCIQCD+ehzrvAuUCJzjRiWUWq7nltenSQNgp7iNHITBtaAtwIhAJGglGd2ipXU9LY5bsYWIBd/Y6zIof3frlEJZ8RymPadAiB3XjYNTBEZEonWkavHaeRrxhNGREMzpQ0n89+premdiQIgdfnlGoHZ0NBmLPaOZ/xalAGIT/9KcYfLuSVDeQNHmYA=-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOAIBAAJAVlkKWKvK0CamhS0m/H2qcGuhPLejVJQDco09xPjADKinZBMdCzJGuwPvp1DETnRXJcaY3XV886S2ZTwGc2WSjQIDAQABAkAdXYq3PTVsNLjM8d4gYFxmfzQGgi15xhymCILJSg+GPi8hfX9MhUt7dMD8JK4L+jm2WL0MZtPP/hQPlSS5TFABAiEAqjee9GigGYmp2x0OjOAJ2ofRA0Pq52jsrPHtnYsTQcECIQCB3RnqZ1ek0oVO+HWOiJnV7PRxVjwb/NJ0QEtrf46rzQIgQu0rqp2YlXHzPxvlpK5SMYJFRCtoKSSwKi/VZaRRJgECIFSBIdoMgVW5wBGRUD392c0BMleF+/tiWzhWlj1PdfGJAiAkQYJVejq1BEtPQSUd4wt+LsQ4cG8gHaXkZQdUbONSwg==-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOwIBAAJBALCMtwuByz1uW6CPuhHAfIac4kjGPkpx8iMpZroKzhuOcEyF48MG4433tC663t9/RE7pBFKMdLONQxXiRSxb2B0CAwEAAQJBAJB31SE5aJIwSBIvCleUZ1HP9usOLdLFQxq6ujDqvpWUrtAAO5QVfxz7jAdJtny3IreNT4uTTB6u2RpOMiPtuIECIQDuxzW+WHDHuQGWJNJ8Z7Mkzq/XqIvrLYrB0zfbH3SB+QIhAL1IhU6oshYD69IRNm87B+kgQRV6pzvRiJGF/wn5z1BFAiBAUwwoXfzr4OwLfZnyh6Eb/k4io7YlwdwKgRMgBaZGSQIge295j4wvZm/0Ly+OYgcK55pjFxC/UiEYgF3zrZu8+EkCIQClbYxLKRhhHf5EYqSkUfspMqaLWVp3bIeYr7o650ldzA==-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOwIBAAJBAKFfG2P43pFlYrn9Uyck5v2+ZCPJJzespnrd37FvFMO8/PvGjuQqQtQ/X0BJDeAR9gwIyvCzAvZvy+gk2XXUUKsCAwEAAQJAARQXpMVnB2W8ES4mPh67BHsfRDX18yNOQYf2FMN/SgeYlVLTcHvepC6+/lfZRmr8ZquMZuG9VBieNuVdr+b6EQIhAON9fQ4DC8Wnpw1Hu5jSoId6x9b5myIQCOBqgpa5nx11AiEAtZhZbD3etUDTP8zPJD03rLkHDEL+CejFbFv6wpQTUZ8CIQCqe66uQ4Yc+Elu4GUsYBzBqzf8WU7zNgUQ+YEoJ6+gyQIhAJcqUk4rlqe1ZeT3PfvBxgXPJJHNdqGTFjzkuHKfRxGrAiAHnrVGreHDPE/GAlTt/LygjSy3Rfix+X6uK5+BwHVUrA==-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOwIBAAJBAKFfG2P43pFlYrn9Uyck5v2+ZCPJJzespnrd37FvFMO8/PvGjuQqQtQ/X0BJDeAR9gwIyvCzAvZvy+gk2XXUUKsCAwEAAQJAARQXpMVnB2W8ES4mPh67BHsfRDX18yNOQYf2FMN/SgeYlVLTcHvepC6+/lfZRmr8ZquMZuG9VBieNuVdr+b6EQIhAON9fQ4DC8Wnpw1Hu5jSoId6x9b5myIQCOBqgpa5nx11AiEAtZhZbD3etUDTP8zPJD03rLkHDEL+CejFbFv6wpQTUZ8CIQCqe66uQ4Yc+Elu4GUsYBzBqzf8WU7zNgUQ+YEoJ6+gyQIhAJcqUk4rlqe1ZeT3PfvBxgXPJJHNdqGTFjzkuHKfRxGrAiAHnrVGreHDPE/GAlTt/LygjSy3Rfix+X6uK5+BwHVUrA==-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOQIBAAJBAIcXUudSjFxCS7LzocoS7AeYBorv7aLYx0v+FnDg/8ZYTS61naYByKmpmjpNhkSIBCGM1IHhbdJqy5vv+nDGTAECAwEAAQJAOwAdzjgIY3b09thfThFLktl76gx1/kytZmw3wt8R9r8QPzLID4dNxYMFQ+d+fpQaajWCYBX0Px5j+ViFEySjPQIhANRWnd8bMWajfJKoFH60PzlWkjrSWC11YazAjRFOKn0fAiEAot5zmrXbhjZuC2HGq3odukC+S65Vr+bPXoWOhGUE8t8CIBHqxSOATejVPru5RxFx8CvMbEZeXWZokzdagAzHxboNAiA/R3zVFGlZiRZSd8fP4h9guTz2fKKwOFZDnFKWIAW/MwIgd9t3uNbT/qulZmq3IrEC820gqpxpTzYMWmsJpEeydoc=-----END RSA PRIVATE KEY-----',
-	'-----BEGIN RSA PRIVATE KEY-----MIIBOQIBAAJAY22qt3TbmmJNOdtQrPcuTVyYcUnFn6MgPr+RpSc8/rkPllLZc7KRFgmTfGww8ajQ3+Qshp1UaYGDy5+6vKL8CwIDAQABAkBIvleEWU7BqOHvTnG72ixaFUDCwcKAhQx2XBnmxsptYXUX8UH2QFBsxbfZa2qtxMBmcc22R/sr+9V1kKIo9hWhAiEAsgCcnPzMOm6SkSEkjxhxzeBzRaDMa/eKJ7HlYJ2KomUCIQCO/weHtLbvJ6gE3CCXkdg4Wb7qkhF7cSN+8mDOSsoFrwIgQNJMNjcxQE4n/cXuBFZIi0qjb8XA68h3fUEhYO++HqkCID1EGUbUb5hxxjQm2DP4gcpK8VhxMtpPtCSc1ksskffzAiEAkrGiD/LvHH89lv2DZw2k3ttbwq5r9xVjHhsIg2bOGfA=-----END RSA PRIVATE KEY-----'
-)
+
+tarjetas = [
+	{
+      "cardNumber": "4716 0445 8959 4204",
+      "holder": "Hermione Kelsy",
+      "expires": "10/22",
+      "cvv": "861"
+    },
+    {
+      "cardNumber": "4916 0901 3687 1031",
+      "holder": "Evonne Matty",
+      "expires": "5/24",
+      "cvv": "419"
+    },
+    {
+      "cardNumber": "4716 3812 0486 4772",
+      "holder": "Melisse Gaylene",
+      "expires": "1/23",
+      "cvv": "501"
+    },
+    {
+      "cardNumber": "4539 3174 3238 5045",
+      "holder": "Karola Myranda",
+      "expires": "12/24",
+      "cvv": "946"
+    },
+    {
+      "cardNumber": "4916 1951 2163 2031",
+      "holder": "Shirl Fiona",
+      "expires": "6/23",
+      "cvv": "506"
+    },
+    {
+      "cardNumber": "4916 3867 9657 1972",
+      "holder": "Maxine Manon",
+      "expires": "6/22",
+      "cvv": "271"
+    },
+    {
+      "cardNumber": "4166 2765 8233 6135",
+      "holder": "Cyndy Goldina",
+      "expires": "9/23",
+      "cvv": "466"
+    },
+    {
+      "cardNumber": "4024 0071 2610 9338",
+      "holder": "Linda Nanci",
+      "expires": "3/27",
+      "cvv": "946"
+    },
+    {
+      "cardNumber": "4532 9305 6108 9647",
+      "holder": "Noreen Luciana",
+      "expires": "2/23",
+      "cvv": "806"
+    },
+    {
+      "cardNumber": "4532 6174 7241 2709",
+      "holder": "Kellsie Karissa",
+      "expires": "8/27",
+      "cvv": "424"
+    }
+]
 
 def elegir_materia():
     materia = random.choice(nombres)
     materia.encode()
     return materia
 
-def elegor_rsa():
-	rsa = random.choice(rsapr)
-	rsa.encode()
-	return rsa
+def elegir_tarjeta():
+	# for elemento in tarjetas:
+	# 	for k,v in elemento.items():
+	# 		pass
+	
+	aleatorio = random.choice(tarjetas)
+
+	aleatorio = str(aleatorio)
+
+	return aleatorio
+
 
 def main():
 	for iterable in range(1, 11):
@@ -80,7 +133,7 @@ def main():
 		
 		with open(titulo_txt, "w") as archivo:
 			archivo.write(mensaje*5)
-			archivo.write('\n' + elegor_rsa())	
+			archivo.write('\n' + elegir_tarjeta())
 
 
 if __name__ == '__main__':
