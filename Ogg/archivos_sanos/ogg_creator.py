000dfb142342b7a0d21249a2a0dc0908e0c953cb
edgarperez97 <edgarperez_97@hotmail.com>
2021-11-05 21:14:43 -0600

    se crearon los archivos para ogg

---
+++ Ogg/archivos_sanos/ogg_creator.py
@@ -0,0 +1,15 @@
+#CREAR 50 ARCHIVOS OGG#
+import os, random
+import shutil
+
+def main():
+
+    archivos = os.listdir('./ogg_base')
+
+    for i in range(1, 51):
+        nombre_seleccionado = random.choice(archivos)
+        shutil.copy2(f'./ogg_base/{nombre_seleccionado}',f'{nombre_seleccionado}_{i}.ogg')
+
+
+if __name__ == '__main__':
+	main()
