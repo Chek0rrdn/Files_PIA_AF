7d9200015e33a2bc02e4c3bb670360426000e886
th3edger <edgarperez_97@hotmail.com>
2022-05-04 13:38:40 -0500

    modificacion menor a Jpg/archivos_sanos/jpg_creator.py

--- Jpg/archivos_sanos/jpg_creator.py
+++ Jpg/archivos_sanos/jpg_creator.py
@@ -1,14 +1,24 @@
 #CREAR 50 IMAGENES#
-import os, random
+import os
+import pathlib
+import random
 import shutil
 
+CURRENT_DIR = pathlib.Path().resolve()
+
 def main():
-    ruta = "/home/edgar/Dev/Files_PIA_AF/Jpg/archivos_sanos/imagenes"
-    archivos = os.listdir(ruta)
+
+    if not CURRENT_DIR.joinpath("imagenes").exists():
+        CURRENT_DIR.joinpath("imagenes").mkdir()
+
+    if not CURRENT_DIR.joinpath("imagenes", "imagenes_2").exists():
+        CURRENT_DIR.joinpath("imagenes_2").mkdir()
+
+    archivos = os.listdir(CURRENT_DIR.joinpath("imagenes"))
     
     for i in range(1, 51):    
         imagen_seleccionado = random.choice(archivos)
-        shutil.copy2(f'./imagenes/{imagen_seleccionado}', f'imagen_seleccionado_{i}.jpg')
+        shutil.copy2(CURRENT_DIR.joinpath("imagenes", f"{imagen_seleccionado}"), CURRENT_DIR.joinpath("imagenes_2",f"img_{i}.jpg"))
 
 if __name__ == '__main__':
 	main()
